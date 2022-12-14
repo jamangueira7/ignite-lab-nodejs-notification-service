04392d1932f7492c338c2206059037c195a7e285
João Mangueira <jamjs7@gmail.com>
2022-12-14 11:29:59 -0300

    primeira reorganização das pastas

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './infra/app.controller';
+import { AppService } from './infra/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
---
+++ src/application/entities/notification.ts
@@ -0,0 +1,7 @@
+export class Notification {
+    private content: string;
+    private category: string;
+
+    constructor() {
+    }
+}
