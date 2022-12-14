c0f6e128207210d39bf9b23c0c3ea603c424174c
João Mangueira <jamjs7@gmail.com>
2022-12-14 11:58:56 -0300

    criando a entendidade notification

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,7 +1,53 @@
-export class Notification {
-    private content: string;
-    private category: string;
+import { Content } from './content';
 
-    constructor() {
-    }
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private _props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this._props = props;
+  }
+
+  public set content(content: Content) {
+    this._props.content = content;
+  }
+
+  public get content(): Content {
+    return this._props.content;
+  }
+
+  public set category(category: string) {
+    this._props.category = category;
+  }
+
+  public get category(): string {
+    return this._props.category;
+  }
+
+  public set recipientId(recipientId: string) {
+    this._props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this._props.recipientId;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this._props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this._props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this._props.createdAt;
+  }
 }
